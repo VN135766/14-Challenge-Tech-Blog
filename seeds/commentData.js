cbc979529477cbee92d363809efcc5521cb826ff
Victor Nunez <VN135766@gmail.com>
2022-08-20 21:07:28 -0400

    comments now working

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,13 +2,11 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        id:1,
         content: 'ok i understand now',
         user_id: 1,
         post_id: 2,
     },
     {
-        id:2,
         content: 'i still dont get it',
         user_id: 2,
         post_id: 1,
